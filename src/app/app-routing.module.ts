a3f753e54a15bbfcf9a6ff17c62b5725e299257c
prueba <42043332+dacardo4@users.noreply.github.com>
2020-11-22 22:45:49 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaComponent } from './componentes/lista/lista.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
