ba374af736d0b2e9c32144a394405d29a8aa0d33
prueba <42043332+dacardo4@users.noreply.github.com>
2021-01-11 10:37:18 -0500

    Se actualiza el directorio

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const express = require('express');
+
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/myMarket'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/myMarket/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
