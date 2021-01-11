f1fe6327a08027b4c5c964b9a45ea92afd524b8d
prueba <42043332+dacardo4@users.noreply.github.com>
2021-01-11 11:24:27 -0500

    Se actualiza el tag del componmente lista

--- src/app/componentes/lista/lista.component.ts
+++ src/app/componentes/lista/lista.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-lista',
+  selector: 'listaProductos',
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.scss']
 })
