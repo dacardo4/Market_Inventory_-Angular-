80bce2ab3bd34d7a7003df055f3265da99716432
prueba <42043332+dacardo4@users.noreply.github.com>
2021-01-11 12:02:36 -0500

    Se visualiza la lista con las datos de la DB

--- src/app/componentes/lista/lista.component.ts
+++ src/app/componentes/lista/lista.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { CantidadService } from 'src/app/servicios/cantidad.service';
+import { ProductoService } from 'src/app/servicios/producto.service';
 
 @Component({
   selector: 'listaProductos',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
-  constructor() { }
+  allProductos = [];
+  allCantidades = [];
+  constructor(
+    private _productoService: ProductoService,
+    private _cantidadService: CantidadService,
+  ) {
+    this.getProductos();
+    this.getCantidades();
+  }
 
   ngOnInit(): void {
   }
 
+  getProductos(): void {
+    this._productoService.getProducto().pipe(take(1)).subscribe(
+      data => {
+        console.log(data);
+        this.allProductos = data;
+      }, error => {
+        console.log('getProductos Error: ',error);
+      }
+    );
+  }
+
+  getCantidades(): void {
+    this._cantidadService.getCantidad().pipe(take(1)).subscribe(
+      data => {
+        console.log(data);
+        this.allCantidades = data;
+      }, error => {
+        console.log('getProductos Error: ',error);
+      }
+    );
+  }
+
+  nombreProducto(idProducto: number): string {
+    let nombre = '';
+    console.log(idProducto);
+    nombre = this.allProductos[idProducto]['productName'];
+    console.log(nombre);
+    return nombre;
+  }
 }
